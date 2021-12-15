9447680f580b711520e1a596a4b0e108e3e828b5
ForSureN1 <standin1532@gmail.com>
2021-12-15 18:09:20 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -79,6 +79,25 @@ $('.partners__items').slick({
     nextArrow: '<button type="button" class="slick_next part_arrow"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_367_95)"><path d="M3.47467 17.6417L11.1163 10L3.47467 2.35834L5.83301 0L15.833 10L5.83301 20L3.47467 17.6417Z" fill="white"/></g><defs><clipPath id="clip0_367_95"><rect width="20" height="20" fill="white" transform="matrix(0 -1 -1 0 20 20)"/></clipPath></defs></svg></button>',
     autoplay: true,
     speed: 1500,
+    responsive: [{
+        breakpoint: 1100,
+        settings: {
+            slidesToShow: 3,
+        }
+    },
+    {
+        breakpoint: 700,
+        settings: {
+            slidesToShow: 2,
+        }
+    },
+    {
+        breakpoint: 500,
+        settings: {
+            slidesToShow: 1,
+        }
+    }
+]
 });
 
 
